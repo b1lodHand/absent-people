1269e455b8f904616898d1f6f29a521339f96675
absence <usturakeserway@gmail.com>
2024-05-02 16:06:23 +0300

    update
    icons

--- Runtime/Person.cs
+++ Runtime/Person.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace com.absence.personsystem
 {
-    [CreateAssetMenu(menuName = "Person", fileName = "New Person")]
+    [CreateAssetMenu(menuName = "absencee_/Person", fileName = "New Person")]
     public class Person : ScriptableObject
     {
         public string Name;
